849fa73147a0e1e5d3f5e01c7bf821a41d60f7bd
logan <loganlenevez@gmail.com>
2024-11-05 14:43:10 +0100

    Add docstring trought my image_processor

--- src/image_processor.py
+++ src/image_processor.py
@@ -4,7 +4,16 @@ from PIL import Image
 
 
 class ImageProcessor:
+
     def __init__(self, folder_path: str):
+        """
+        Sets the input folder path and creates a timestamped output folder for processed images.
+
+        Parameters:
+        -----------
+        folder_path : str
+            Path to the folder containing images to process.
+        """
         self.folder_path = folder_path
         self.output_folder = os.path.join(
             "datasets", datetime.now().strftime("%Y%m%d%H%M%S")
@@ -12,11 +21,29 @@ class ImageProcessor:
         os.makedirs(self.output_folder, exist_ok=True)
 
     def process_folder(self, size: int):
+        """
+        Processes all .jpg files in the folder to a max dimension, adding padding if needed.
+
+        Parameters:
+        -----------
+        size : int
+            Maximum dimension for the longer side of each image.
+        """
         for filename in os.listdir(self.folder_path):
-            if filename.lower().endswith((".jpg",)):
+            if filename.lower().endswith(".jpg"):
                 self.process_image(filename, size)
 
     def process_image(self, filename: str, size: int):
+        """
+        Resizes and adds padding to an image, saving it to the output folder.
+
+        Parameters:
+        -----------
+        filename : str
+            Name of the image file to process.
+        size : int
+            Maximum dimension for resizing.
+        """
         img_path = os.path.join(self.folder_path, filename)
         with Image.open(img_path) as img:
             img = self.resize_image(img, size)
@@ -25,16 +52,42 @@ class ImageProcessor:
             img.save(output_path)
 
     def resize_image(self, img: Image.Image, size: int) -> Image.Image:
+        """
+        Resizes an image while maintaining its aspect ratio.
+
+        Parameters:
+        -----------
+        img : Image.Image
+            Image to resize.
+        size : int
+            Maximum dimension for the longest side.
+
+        Returns:
+        --------
+        Image.Image
+            Resized image.
+        """
         width, height = img.size
         aspect_ratio = min(size / width, size / height)
         new_width = int(width * aspect_ratio)
         new_height = int(height * aspect_ratio)
 
-        img.resize((new_width, new_height), Image.LANCZOS)
-
-        return img
+        return img.resize((new_width, new_height), Image.LANCZOS)
 
     def add_padding(self, img: Image.Image) -> Image.Image:
+        """
+        Adds padding to make an image square if it’s not already.
+
+        Parameters:
+        -----------
+        img : Image.Image
+            Image to add padding to.
+
+        Returns:
+        --------
+        Image.Image
+            Square image with padding if needed.
+        """
         background_color = (114, 114, 144)
         width, height = img.size
         if width == height:
@@ -42,8 +95,8 @@ class ImageProcessor:
         elif width > height:
             new_img = Image.new("RGB", (width, width), background_color)
             new_img.paste(img, (0, (width - height) // 2))
-            return new_img
         else:
             new_img = Image.new("RGB", (height, height), background_color)
             new_img.paste(img, ((height - width) // 2, 0))
-            return new_img
+
+        return new_img
